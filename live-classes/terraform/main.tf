ae05121e1b46532274779a1d0da2f544df2fd2b7
Akas Patranobis <akaspatranobis@yahoo.com>
2025-08-17 12:25:42 +0530

    up

---
+++ live-classes/terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-dsdhsdjhsgdjhsgdjsgdsjgdsjdgsjd"
+  force_destroy = false
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
